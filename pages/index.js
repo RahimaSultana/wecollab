709733058f24b649bb88e716d1bd2bdcb3109e65
unknown <heenavinayak91@gmail.com>
2022-11-20 22:23:10 -0500

    updated the responsiveness

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,15 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className='container'>
-   
+   <div className="intro">
 <h1>Welcome to WeCollab App</h1>
-<a href='/allposts'>
-   Lets Get Started </a>
+<p>Welcome to WeCollab where people share anything and everything to feel good</p>
+<button className="btn">
+<Link href='/allposts' >
+   Lets Get Started
+   </Link>
+   </button>
+   </div>
   </div>
   )
 }
